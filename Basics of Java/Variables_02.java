c94649ba8f56f924c6a632a3e4ded22f05262179
Shafi073 <shafiaaghaz073@gmail.com>
2025-11-12 22:06:28 +0530

    About Variables in java

---
+++ Basics of Java/Variables_02.java
@@ -0,0 +1,13 @@
+public class Variables_02 {
+    public static void main(String args[]){
+        int a = 10; /*here both a and b are variables and their values can be changed. */
+        int b = 5;
+        System.out.println(a);
+        System.out.println(b);
+        /*we can also assign one varibale value to another */
+        String name = "Tony Stark";
+        System.out.println(name);
+
+    }
+    
+}
