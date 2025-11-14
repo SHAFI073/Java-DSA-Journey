a5a3d541f217e20c091d7eecad22291a50b08fb0
Shafi073 <shafiaaghaz073@gmail.com>
2025-11-14 22:00:02 +0530

    odd even program

---
+++ Basics of Java/Conditional_If_elseQ1_03.java
@@ -0,0 +1,16 @@
+/*Odd Even */
+import java.util.*;
+public class Conditional_If_elseQ1_03 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        
+        if( num % 2 == 0){
+            System.out.println("Entered number is even");
+        }
+        else{
+            System.out.println("Number is odd");
+        }
+        sc.close();
+    }
+}
