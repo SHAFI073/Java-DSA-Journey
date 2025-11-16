74d524db73e86e6e90bfae0422fa7c680f52c057
Shafi073 <shafiaaghaz073@gmail.com>
2025-11-16 15:33:29 +0530

    reverse of a number

---
+++ Basics of Java/ForLoopQ2_04.java
@@ -0,0 +1,18 @@
+/*Reverse a number */
+import java.util.*;
+public class ForLoopQ2_04 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number :");
+        int n = sc.nextInt();
+        int rev = 0;
+        while( n > 0){
+            int lastDigit = n % 10;
+            rev = (rev * 10) + lastDigit;
+            n = n / 10;
+        }
+        System.out.print(rev);
+        sc.close();
+
+    }
+}
