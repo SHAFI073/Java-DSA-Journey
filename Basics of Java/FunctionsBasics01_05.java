e3e191766042429772f78c359f70f701aa9cc595
Shafi073 <shafiaaghaz073@gmail.com>
2025-11-19 13:57:21 +0530

    Basic working of Functions

---
+++ Basics of Java/FunctionsBasics01_05.java
@@ -0,0 +1,10 @@
+public class FunctionsBasics01_05 {
+    public static void HelloWorld(){
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+    public static void main(String args[]){
+        HelloWorld();
+    }
+}
