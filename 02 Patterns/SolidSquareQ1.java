1a4b397990da499259e1f2e944f093b68d4c734f
Shafi073 <shafiaaghaz073@gmail.com>
2025-11-26 17:17:33 +0530

    Solid square pattern

---
+++ 02 Patterns/SolidSquareQ1.java
@@ -0,0 +1,13 @@
+public class SolidSquareQ1 {
+    public static void square(){
+        for( int i = 0; i < 6; i++){
+            for( int j = 0; j < 6; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+ public static void main(String args[]){
+    square();
+ }   
+}
