872d58cfcffc3c7b3b428be73c04e9b5ea1055e8
Shafi073 <shafiaaghaz073@gmail.com>
2026-04-27 14:49:12 +0530

    basics of 2D arrays

---
+++ 07 2D Array/Basics01.java
@@ -0,0 +1,20 @@
+
+
+public class Basics01 {
+    public static void main(String args[]){
+        //int arr[][] = new int[3][4];
+        int arr[][] = {
+                        {1,2},
+                        {3,4},
+                        {5,6},
+                        {7,8}
+                    };
+        for( int row = 0; row < arr.length; row++){
+            for( int column = 0; column < arr[row].length; column++){
+                System.out.print(arr[row][column]+" ");
+
+            }
+            System.out.println();
+        }
+    }
+}
